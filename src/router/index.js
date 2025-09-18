c0133c7af3d05e3dc560a223a9364c9ac11b1041
juao99 <jpsshome@gmail.com>
2025-09-18 18:08:07 -0300

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
-    history: createWebHistory('/portifolio/'),
+    history: createWebHistory('/JoaoPedro/'),
     routes: [
         {
             path: '/',
--- vite.config.mjs
+++ vite.config.mjs
@@ -16,7 +16,7 @@ export default defineConfig({
             resolvers: [PrimeVueResolver()]
         })
     ],
-    base: '/portifolio/',
+    base: '/JoaoPedro/',
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
